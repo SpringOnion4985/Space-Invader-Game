2770093f5ae0754db9cacc6299687a53048a036a
SpringOnion <nirbhay5813@gmail.com>
2025-09-07 04:10:50 +0530

    Fix bullet firing logic and refactor game loop.

--- game.py
+++ game.py
@@ -1,69 +1,125 @@
 import pygame
+import math
 import random
 
 pygame.init()
 screen= pygame.display.set_mode((800,600))
 
+background=pygame.image.load('background.png')
+background=pygame.transform.scale(background,(800,600))
+
 pygame.display.set_caption("Space Invaders")
 icon=pygame.image.load('spaceship.png')
 pygame.display.set_icon(icon)
 
+#Positions of player
 playerImg=pygame.image.load('spaceship.png')
 playerImg=pygame.transform.scale(playerImg,(64,64))
-enemyImg=pygame.image.load('enemy.png')
-enemyImg=pygame.transform.scale(enemyImg,(64,64))
 playerX=370
 playerY=480
 playerX_change=0
-playerY_change=0
-enemyX=random.randint(0,736)
-enemyY=random.randint(50,150)
+
+#positions of enemy
+enemy=[]
+enemyX=[]
+enemyY=[]
+enemyX_change=[]
+enemyY_change=[]
+num_of_enemies=6
+
+for i in range(num_of_enemies):
+  enemyImg=pygame.image.load('enemy.png')
+  enemyImg=pygame.transform.scale(enemyImg,(64,64))
+  enemy.append(enemyImg)
+  enemyX_change.append(0.3)
+  enemyY_change.append(40)
+  enemyX.append(random.randint(0,736))
+  enemyY.append(random.randint(50,150))
+
+#positions of bullet
+bulletImg=pygame.image.load('bullet.png')
+bulletImg=pygame.transform.scale(bulletImg,(32,32))
+bulletX_change=0
+bulletY_change=1
+bulletX=0
+bulletY=480
+bullet_state="ready"
+
+score=0
 
 def player(x,y):
   screen.blit(playerImg,(x,y))
 
-def enemy(x,y):
-  screen.blit(enemyImg,(x,y))
+def enemy(x[i],y[i],i):
+  screen.blit(enemyImg,(x[i],y[i]))
 
+def fire_bullet(x,y):
+  global bullet_state
+  bullet_state="fire"
+  screen.blit(bulletImg,(x+16,y+10))
+
+def isCollision(enemyX[i],enemyY[i], bulletX, bulletY):
+  distance=math.sqrt(math.pow(enemyX-bulletX,2)+math.pow(enemyY-bulletY,2))
+  if distance<27:
+    return True
+  else:
+    return False
+
+
+#Game Loop
 running=True
 while running:
   screen.fill((0,0,0))
+  screen.blit(background,(0,0))
   for event in pygame.event.get():
     if event.type==pygame.QUIT:
       running=False
 
     if event.type==pygame.KEYDOWN:
-      print("A keystroke has been pressed")
       if event.key==pygame.K_LEFT:
-        print("Left arrow key is pressed")
-        playerX_change=-0.3
+        playerX_change=-0.5
       if event.key==pygame.K_RIGHT:
-        print("Right arrow key is pressed")
-        playerX_change=0.3
-      if event.key==pygame.K_UP:
-        print("Up arrow key is pressed")
-        playerY_change=-0.3
-      if event.key==pygame.K_DOWN:
-        print("Down arrow key is pressed")
-        playerY_change=0.3
-
+        playerX_change=0.5
+      if event.key==pygame.K_SPACE:
+        if bullet_state=="ready":
+          bulletX=playerX
+          fire_bullet(bulletX,bulletY)
+      
     if event.type==pygame.KEYUP:
-      if event.key==pygame.K_LEFT or event.key==pygame.K_RIGHT or event.key==pygame.K_UP or event.key==pygame.K_DOWN:
-        print("Key stroke has been released")
+      if event.key==pygame.K_LEFT or event.key==pygame.K_RIGHT:
         playerX_change=0
-        playerY_change=0
-
+       
+  # Updating the position of player and enemy
   playerX+=playerX_change
   if playerX<=0:
     playerX=0
   elif playerX>=736:
     playerX=736
 
-  playerY+=playerY_change
-  if playerY<=0:
-    playerY=0
-  elif playerY>=536:
-    playerY=536
+  enemyX+=enemyX_change
+  if enemyX<=0:
+    enemyX_change=0.3
+    enemyY+=enemyY_change
+  elif enemyX>=736:
+    enemyX_change=-0.3
+    enemyY+=enemyY_change
+
+  if bullet_state=="fire":
+    fire_bullet(bulletX,bulletY)
+    bulletY-=bulletY_change
+    if bulletY<=0:
+      bulletY=480
+      bullet_state="ready"
+
+  collision=isCollision(enemyX, enemyY, bulletX, bulletY)
+  if collision:
+    bulletY=480
+    bullet_state="ready"
+    score+=1
+    print(score)
+    enemyX=random.randint(0,736)
+    enemyY=random.randint(50,150)
+
   enemy(enemyX,enemyY)
   player(playerX,playerY)
   pygame.display.update()
